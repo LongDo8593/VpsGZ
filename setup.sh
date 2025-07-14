f73b9e4bfec06e87a51bfe519cc9b1abd55a4c07
ChocolateV24 <155208108+LongDo8593@users.noreply.github.com>
2025-07-14 17:03:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
-services:
-  windows:
-    image: dockurr/windows
-    container_name: windows
-    environment:
-      VERSION: "win10"
-      RAM_SIZE: "12G"
-      CPU_CORES: "2"
-    devices:
-      - /dev/kvm
-    cap_add:
-      - NET_ADMIN
-    ports:
-      - "8006:8006"
-      - "3389:3389/tcp"
-      - "3389:3389/udp"
-    stop_grace_period: 2m
+#!/bin/bash
+sudo apt update && sudo apt install qemu-system -y
+qemu-img create -f raw 2012r2.img 20G
+wget -O- --no-check-certificate http://drive.muavps.net/windows/Windows2012r2.gz | gunzip | dd of=2012r2.img
+qemu-img resize 2012r2.img 300G
+qemu-system-x86_64 \
+-net nic -net user,hostfwd=tcp::3389-:3389 \
+-m 16G -smp cores=8 \
+-cpu max \
+-enable-kvm \
+-boot order=d \
+-drive file=2012r2.img,format=raw,if=virtio \
+-device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+-device usb-tablet \
+-device virtio-gpu \
+-vga virtio \
+-display gtk
